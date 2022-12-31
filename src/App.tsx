078d36e491de45f86b4de347504c1044e9147d75
shenafield <99119344+shenafield@users.noreply.github.com>
2022-12-31 18:53:47 +0200

    Added the joining article

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,35 @@ import {
   Link
 } from 'react-router-dom'
 
+function Joining() {
+  return (
+    <article>
+      <h1>Joining the following</h1>
+      <p>
+        Find the server: To join the Barbie following, you will need to first find the Discord server where the community is located. You can do this by asking around, searching online, or by looking for server recommendations from friends or other community members.
+      </p>
+      <p>
+        Request an invitation: Once you have found the server, you will need to request an invitation to join. This is usually done by reaching out to the server owner or one of the moderators and asking to be added to the community.
+      </p>
+      <p>
+        Wait for approval: After you have sent your request, you will need to wait for approval from the server owner or moderators. They will review your request and decide whether or not to allow you to join the community.
+      </p>
+      <p>
+        Accept the invitation: If your request is approved, you will receive an invitation to join the server. To join, you will need to click on the link provided in the invitation and follow the instructions to complete the process.
+      </p>
+      <p>
+        Introduce yourself: Once you have joined the server, you will need to introduce yourself to the community. This is usually done by posting a message in the introductions channel, telling the other members a little bit about yourself and your interests.
+      </p>
+      <p>
+        Participate in the community: After you have introduced yourself, you can start participating in the community by joining conversations, sharing your thoughts and ideas, and getting to know the other members. You can also participate in activities, events, and projects organized by the server owner or the community members.
+      </p>
+      <p>
+        By following these steps, you can join the Barbie following and become an active member of the community.
+      </p>
+    </article>
+  )
+}
+
 function MainPage() {
   return (
     <article>
@@ -26,7 +55,7 @@ function MainPage() {
       <p>
         The members were thrilled with the transformation and loved the new, playful and girly atmosphere of the server 💕. They enjoyed chatting with each other about their favorite dolls 💕, sharing their creative ideas 💡, and participating
       </p>
-      <button type="button">Join the following</button>
+      <Link to="/joining"><button type="button">Join the following</button></Link>
     </article>
   )
 }
@@ -36,6 +65,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/joining" element={<Joining />} />
       </Routes>
     </Router>
   )
