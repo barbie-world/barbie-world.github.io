557f6813f0ac3340896861ffedac1d3016a8a5e3
shenafield <99119344+shenafield@users.noreply.github.com>
2022-12-31 18:29:49 +0200

    Removed template

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.sass'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <article>
+      <h1>I'm a barbie girl</h1>
+      <p>
+        Lorem ipsum dolor sit <a href="#">amet consectetur</a> adipisicing elit. Ipsam quam assumenda facere perferendis earum! Esse, fugit. Cumque omnis magni ipsam rem, architecto ipsa, ut est provident sequi voluptates iste aliquid.
       </p>
-    </div>
+      <button type="button">Click me</button>
+    </article>
   )
 }
 
